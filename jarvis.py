6e773f3e761d293765a7161b2db1d97b0f87c5f5
Sanchari14 <118428302+Sanchari14@users.noreply.github.com>
2023-11-21 00:18:15 +0530

    day11

--- jarvis.py
+++ jarvis.py
@@ -99,6 +99,6 @@ if __name__ == "__main__":
                 print(e)
                 speak("Sorry, I am not able to send this email")    
                 
-                if == == ok
+               
    
     
